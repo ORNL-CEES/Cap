e4321247d490ac7ce4965162b607a40aaa4b7690
Damien L-G <qdi@ornl.gov>
2014-12-05 14:47:44 -0500

    cleaning the tests

---
+++ test/common.cc
@@ -0,0 +1,78 @@
+#include <boost/property_tree/ptree.hpp>
+
+void put_default_parameters(boost::property_tree::ptree & params)
+{
+    params.put("material_properties.separator_heat_capacity",         1.2528e3 );
+    params.put("material_properties.electrode_heat_capacity",         0.93e3   );
+    params.put("material_properties.collector_heat_capacity",         2.7e3    );
+    params.put("material_properties.separator_density",               3.1404e3 );             
+    params.put("material_properties.electrode_density",               1.34e3   );             
+    params.put("material_properties.collector_density",               0.89815e3);             
+    params.put("material_properties.separator_thermal_conductivity",  0.0019e2 );       
+    params.put("material_properties.electrode_thermal_conductivity",  0.0011e2 );       
+    params.put("material_properties.collector_thermal_conductivity",  2.37e2   );       
+
+    params.put("material_properties.alpha",                           0.0      );       
+                                                                     
+    params.put("material_properties.specific_capacitance",           86.0e6    );          
+    params.put("material_properties.separator_void_volume_fraction",  0.6      );
+    params.put("material_properties.electrode_void_volume_fraction",  0.67     );
+    params.put("material_properties.electrolyte_conductivity",        0.067    );      
+    params.put("material_properties.solid_phase_conductivity",       52.1      );      
+
+    params.put("boundary_values.charge_potential",             2.2);      
+    params.put("boundary_values.discharge_potential",          1.4);      
+    params.put("boundary_values.charge_current_density",     324.65);      
+    params.put("boundary_values.discharge_current_density", -324.65);      
+    params.put("boundary_values.initial_potential",            1.6);      
+                                                                  
+    params.put("boundary_values.upper_ambient_temperature",       0.0);      
+    params.put("boundary_values.lower_ambient_temperature",       0.0);      
+    params.put("boundary_values.upper_heat_transfer_coefficient", 8.0e-2);      
+    params.put("boundary_values.lower_heat_transfer_coefficient", 0.0);      
+
+    // USUALLY YOU WILL NOT WANT TO MESS WITH THE THESE ONES
+    params.put("solid_potential_component",  0);
+    params.put("liquid_potential_component", 1);
+    params.put("temperature_component",      2);
+
+    params.put("material_properties.separator_material_id",         2);          
+    params.put("material_properties.anode_electrode_material_id",   1);
+    params.put("material_properties.anode_collector_material_id",   4);
+    params.put("material_properties.cathode_electrode_material_id", 3);      
+    params.put("material_properties.cathode_collector_material_id", 5);      
+
+    params.put("boundary_values.separator_material_id",         2);          
+    params.put("boundary_values.anode_electrode_material_id",   1);
+    params.put("boundary_values.anode_collector_material_id",   4);
+    params.put("boundary_values.cathode_electrode_material_id", 3);      
+    params.put("boundary_values.cathode_collector_material_id", 5);      
+
+    params.put("boundary_values.cathode_boundary_id",           1);
+    params.put("boundary_values.anode_boundary_id",             2);
+    params.put("boundary_values.upper_boundary_id",             3);
+    params.put("boundary_values.lower_boundary_id",             4);
+    params.put("boundary_values.other_boundary_id",             5);
+
+}
+
+std::shared_ptr<boost::property_tree::ptree> initialize_database()
+{
+    std::shared_ptr<boost::property_tree::ptree> database(new boost::property_tree::ptree);
+    database->put("solid_potential_component",     0);
+    database->put("liquid_potential_component",    1);
+    database->put("temperature_component",         2);
+    database->put("electrochemical_block",         0);
+    database->put("thermal_block",                 1);
+    database->put("separator_material_id",         2);          
+    database->put("anode_electrode_material_id",   1);
+    database->put("anode_collector_material_id",   4);
+    database->put("cathode_electrode_material_id", 3);      
+    database->put("cathode_collector_material_id", 5);      
+    database->put("cathode_boundary_id",           1);
+    database->put("anode_boundary_id",             2);
+    database->put("upper_boundary_id",             3);
+    database->put("lower_boundary_id",             4);
+    database->put("other_boundary_id",             5);
+    return database;
+}
--- test/test_constant_current_cycling.cc
+++ test/test_constant_current_cycling.cc
@@ -6,92 +6,24 @@
 #include <string>
 #include <vector>
 
-void put_default_parameters(boost::property_tree::ptree & params)
-{
-    params.put("material_properties.separator_thermal_conductivity", 1.2528e3);
-    params.put("material_properties.electrode_thermal_conductivity", 0.93e3);
-    params.put("material_properties.collector_thermal_conductivity", 2.7e3);
-    params.put("material_properties.separator_density",              3.1404e3);             
-    params.put("material_properties.electrode_density",              1.34e3);             
-    params.put("material_properties.collector_density",              0.89815e3);             
-    params.put("material_properties.separator_heat_capacity",        0.0019e2);       
-    params.put("material_properties.electrode_heat_capacity",        0.0011e2);       
-    params.put("material_properties.collector_heat_capacity",        2.37e2);       
-
-    params.put("material_properties.alpha",                          0.0);       
-                                                                     
-    params.put("material_properties.specific_capacitance",           86.0e6);          
-    params.put("material_properties.separator_void_volume_fraction",  0.6  );
-    params.put("material_properties.electrode_void_volume_fraction",  0.67 );
-    params.put("material_properties.electrolyte_conductivity",        0.067);      
-    params.put("material_properties.solid_phase_conductivity",        52.1 );      
-
-    params.put("material_properties.separator_material_id",         2);          
-    params.put("material_properties.anode_electrode_material_id",   1);
-    params.put("material_properties.anode_collector_material_id",   4);
-    params.put("material_properties.cathode_electrode_material_id", 3);      
-    params.put("material_properties.cathode_collector_material_id", 5);      
-
-    params.put("boundary_values.charge_potential",             2.2);      
-    params.put("boundary_values.discharge_potential",          1.4);      
-    params.put("boundary_values.charge_current_density",     324.65);      
-    params.put("boundary_values.discharge_current_density", -324.65);      
-    params.put("boundary_values.initial_potential",            1.6);      
-                                                                  
-    params.put("boundary_values.upper_ambient_temperature",       0.0);      
-    params.put("boundary_values.lower_ambient_temperature",       0.0);      
-    params.put("boundary_values.upper_heat_transfer_coefficient", 8.0e-2);      
-    params.put("boundary_values.lower_heat_transfer_coefficient", 0.0);      
-
-    params.put("boundary_values.separator_material_id",         2);          
-    params.put("boundary_values.anode_electrode_material_id",   1);
-    params.put("boundary_values.anode_collector_material_id",   4);
-    params.put("boundary_values.cathode_electrode_material_id", 3);      
-    params.put("boundary_values.cathode_collector_material_id", 5);      
-
-    params.put("boundary_values.cathode_boundary_id", 1);
-    params.put("boundary_values.anode_boundary_id",   2);
-    params.put("boundary_values.upper_boundary_id",   3);
-    params.put("boundary_values.lower_boundary_id",   4);
-    params.put("boundary_values.other_boundary_id",   5);
-
-    params.put("time_step",     "1.0");
-    params.put("initial_time",  "0.0");
-    params.put("final_time",   "300.0");
-    params.put("max_cycles",    "100");
-
-    params.put("solid_potential_component",  0);
-    params.put("liquid_potential_component", 1);
-    params.put("temperature_component",      2);
-
-}
+// forward declaration (implementation is in "common.cc")
+std::shared_ptr<boost::property_tree::ptree> initialize_database();
+void put_default_parameters(boost::property_tree::ptree & params);
 
 int main(int argc, char *argv[])
 {
-    std::cout<<"hello world\n";
-
-    std::shared_ptr<boost::property_tree::ptree> database(new boost::property_tree::ptree);
-    database->put("solid_potential_component",     0);
-    database->put("liquid_potential_component",    1);
-    database->put("temperature_component",         2);
-    database->put("electrochemical_block",         0);
-    database->put("thermal_block",                 1);
-    database->put("separator_material_id",         2);          
-    database->put("anode_electrode_material_id",   1);
-    database->put("anode_collector_material_id",   4);
-    database->put("cathode_electrode_material_id", 3);      
-    database->put("cathode_collector_material_id", 5);      
-    database->put("cathode_boundary_id",           1);
-    database->put("anode_boundary_id",             2);
-    database->put("upper_boundary_id",             3);
-    database->put("lower_boundary_id",             4);
-    database->put("other_boundary_id",             5);
-    cap::SuperCapacitorProblem<2> super_capacitor(database);
+    cap::SuperCapacitorProblem<2> super_capacitor(initialize_database());
 
     // SETTING PROBLEM PARAMETERS
     std::shared_ptr<boost::property_tree::ptree> in(new boost::property_tree::ptree);
     put_default_parameters(*in);
     in->put("test_case", 2);
+    in->put("time_step",       "5.0");
+    in->put("initial_time",    "0.0");
+    in->put("final_time",   "1200.0");
+    in->put("max_cycles",    "100"  );
+    in->put("boundary_values.charge_current_density",     32.465);      
+    in->put("boundary_values.discharge_current_density", -32.465);      
 
     // SOLVING THE PROBLEM
     std::shared_ptr<boost::property_tree::ptree> out(new boost::property_tree::ptree);
@@ -101,7 +33,7 @@ int main(int argc, char *argv[])
     std::vector<double> max_temperature = 
         cap::to_vector<double>(out->get<std::string>("max_temperature"));
 
-    std::cout<<"goodbye cruel world\n";
-
     return 0;
 }
+
+#include "common.cc"
