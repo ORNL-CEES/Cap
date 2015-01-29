8f4ba8e01a696475bf679aa54d1ed954add5677a
Damien L-G <qdi@ornl.gov>
2015-01-29 10:42:43 -0500

    small fix to cap.cmake

--- cmake/cap.cmake
+++ cmake/cap.cmake
@@ -9,11 +9,9 @@ EXTERNALPROJECT_ADD(
     CMAKE_ARGS         -D CMAKE_INSTALL_PREFIX=${CAP_INSTALL_DIR}
                        -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
                        -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                       -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                        -D DEAL_II_INSTALL_DIR=${DEAL_II_INSTALL_DIR}
                        -D CAP_DATA_DIR=${CAP_DATA_SOURCE_DIR}
-                       -D NOWPAC_INSTALL_DIR=${NOWPAC_INSTALL_DIR}
-                       -D NLOPT_INSTALL_DIR=${NLOPT_INSTALL_DIR}
-                       -D EIGEN_INSTALL_DIR=${EIGEN_INSTALL_DIR}
     INSTALL_COMMAND    ""
     BUILD_IN_SOURCE    0
     DEPENDS            DEAL_II EIGEN
