1c5852c182f36be35ac147f1e7139cb0a3bd3b47
Damien L-G <qdi@ornl.gov>
2015-02-06 17:15:38 -0500

    use tortuosity rather than bruggemans coeff to compute liquid phase conductivity

--- test/common.cc
+++ test/common.cc
@@ -2,35 +2,37 @@
 
 void put_default_parameters(boost::property_tree::ptree & params)
 {
-    params.put("material_properties.separator_heat_capacity",         1.2528e3 );
-    params.put("material_properties.electrode_heat_capacity",         0.93e3   );
-    params.put("material_properties.collector_heat_capacity",         2.7e3    );
-    params.put("material_properties.separator_density",               3.1404e3 );
-    params.put("material_properties.electrode_density",               1.34e3   );
-    params.put("material_properties.collector_density",               0.89815e3);
-    params.put("material_properties.separator_thermal_conductivity",  0.0019e2 );
-    params.put("material_properties.electrode_thermal_conductivity",  0.0011e2 );
-    params.put("material_properties.collector_thermal_conductivity",  2.37e2   );
+    params.put("material_properties.separator_heat_capacity",            1.2528e3 );
+    params.put("material_properties.electrode_heat_capacity",            0.93e3   );
+    params.put("material_properties.collector_heat_capacity",            2.7e3    );
+    params.put("material_properties.separator_density",                  3.2e3    );
+    params.put("material_properties.electrode_density",                  2.3e3    );
+    params.put("material_properties.collector_density",                  2.70e3   );
+    params.put("material_properties.electrolyte_mass_density",           1.2e3    );
+    params.put("material_properties.separator_thermal_conductivity",     0.0019e2 );
+    params.put("material_properties.electrode_thermal_conductivity",     0.0011e2 );
+    params.put("material_properties.collector_thermal_conductivity",   237.0      );
+    params.put("material_properties.alpha",                              0.0      );
+    params.put("material_properties.differential_capacitance",           0.03134  );
+    params.put("material_properties.separator_void_volume_fraction",     0.6      );
+    params.put("material_properties.electrode_void_volume_fraction",     0.67     );
+    params.put("material_properties.electrolyte_conductivity",           0.067    );
+    params.put("material_properties.solid_phase_conductivity",          52.1      );
+    params.put("material_properties.collector_electrical_resistivity",  28.2e-9   );
+    params.put("material_properties.bruggemans_coefficient",             1.5      );
+    params.put("material_properties.electrode_tortuosity_factor",        2.3      );
+    params.put("material_properties.separator_tortuosity_factor",        1.29     );
+    params.put("material_properties.pores_characteristic_dimension",     1.5e-9   );
+    params.put("material_properties.pores_geometry_factor",              2.0      );
 
-    params.put("material_properties.alpha",                           0.0      );
-                                                                     
-    params.put("material_properties.differential_capacitance",        6.4e-2   );
-    params.put("material_properties.separator_void_volume_fraction",  0.6      );
-    params.put("material_properties.electrode_void_volume_fraction",  0.67     );
-    params.put("material_properties.electrolyte_conductivity",        0.067    );
-    params.put("material_properties.solid_phase_conductivity",       52.1      );
-    params.put("material_properties.bruggemans_coefficient",          1.5      );
-    params.put("material_properties.pores_characteristic_dimension",  1.5e-9   );
-    params.put("material_properties.pores_geometry_factor",           2.0      );
-
-    params.put("boundary_values.charge_potential",                    2.2      );
-    params.put("boundary_values.discharge_potential",                 1.4      );
-    params.put("boundary_values.charge_current_density",            324.65     );
-    params.put("boundary_values.discharge_current_density",        -324.65     );
-    params.put("boundary_values.initial_potential",                   1.6      );
+    params.put("boundary_values.charge_potential",                       2.2      );
+    params.put("boundary_values.discharge_potential",                    1.4      );
+    params.put("boundary_values.charge_current_density",               324.65     );
+    params.put("boundary_values.discharge_current_density",           -324.65     );
+    params.put("boundary_values.initial_potential",                      1.6      );
                                                                   
-    params.put("boundary_values.ambient_temperature",       300.0   );
-    params.put("boundary_values.heat_transfer_coefficient",   8.0e-2);
+    params.put("boundary_values.ambient_temperature",                  300.0      );
+    params.put("boundary_values.heat_transfer_coefficient",              8.0e-2   );
 
     // USUALLY YOU WILL NOT WANT TO MESS WITH THE THESE ONES
     params.put("solid_potential_component",  0);
