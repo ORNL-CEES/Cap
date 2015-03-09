8e12df43f2fd8ac1df603ce50774423df0e4c5a8
Damien L-G <qdi@ornl.gov>
2015-03-09 16:55:10 -0400

    test butler kinetics

--- test/test_butler_volmer_kinetics.cc
+++ test/test_butler_volmer_kinetics.cc
@@ -1,247 +1,68 @@
 #define BOOST_TEST_MODULE ButlerVolmerKinetics
 #define BOOST_TEST_MAIN
-#include <cap/resistor_capacitor.h>
 #include <boost/test/unit_test.hpp>
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 #include <boost/property_tree/ptree.hpp>
-#include <string>
-#include <tuple>
+#include <boost/property_tree/xml_parser.hpp>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
 
+double const TOLERANCE = 1.0e-6;
 
-double const I          =  0.006;
-double const U          =  2.1;
-double const R_SERIES   = 55.0e-3;
-double const R_PARALLEL =  2.5e6;
-double const C          =  3.0;
-double const P          =  0.0017;
-double const TOLERANCE  = 1.0e-8;    // in percentage units
 
-std::shared_ptr<boost::property_tree::ptree> initialize_database()
+BOOST_AUTO_TEST_CASE( test_butler_volmer_kinetic )
 {
+    double const faraday_constant                     =   9.64853365e4;
+    double const gas_constant                         =   8.3144621;
+    double const boltzman_constant                    =   8.6173324e-5;
+    double const temperature                          = 300.0;
+    double const anodic_charge_transfer_coefficient   =   0.5;
+    double const cathodic_charge_transfer_coefficient =   0.5;
+    double const exchange_current_density             =   1.0;
+    BOOST_CHECK_CLOSE(
+        faraday_constant/(gas_constant*temperature),
+        1.0/(boltzman_constant*temperature),
+        TOLERANCE);
+    std::cout<<boost::format("  %15.7e  %15.7e  \n")
+        % static_cast<double>(faraday_constant/(gas_constant*temperature))
+        % static_cast<double>(1.0/(boltzman_constant*temperature))
+        ;
+
+
+
+    std::fstream fout;
+    fout.open("butler_volmer_kinetics_data", std::fstream::out);
+
     std::shared_ptr<boost::property_tree::ptree> database =
         std::make_shared<boost::property_tree::ptree>();
-    database->put("series_resistance"  , R_SERIES  );
-    database->put("parallel_resistance", R_PARALLEL);
-    database->put("capacitance"        , C         );
-    return database;
-}
+    read_xml("input_butler_volmer_kinetics", *database);
 
-BOOST_AUTO_TEST_CASE( test_series_rc_constant_voltage )
-{
-    double const TAU     = R_SERIES * C;
-    double const DELTA_T = 0.1 * TAU;
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
-        time.push_back(t);
-
-    cap::SeriesRC rc(std::make_shared<cap::Parameters>(initialize_database()));
-
-    // CHARGE
-    BOOST_FOREACH(double const & t, time)
+    double const overpotential_lower_limit = database->get<double>("overpotential_lower_limit");
+    double const overpotential_upper_limit = database->get<double>("overpotential_upper_limit");
+    int    const n_points                  = database->get<int   >("n_points"                 );
+    double const step = (overpotential_upper_limit - overpotential_lower_limit) / (n_points + 1);
+    for (double overpotential = overpotential_lower_limit;
+        overpotential <= overpotential_upper_limit;
+        overpotential += step)
     {
-        BOOST_CHECK_CLOSE(rc.U_C, U * (1.0 - std::exp(-t/TAU)), TOLERANCE);
-        rc.evolve_one_time_step_constant_voltage(DELTA_T, U);
+        fout<<boost::format("  %10.7f  %15.7e  %15.7e  \n")
+            % overpotential
+            % static_cast<double>(
+                exchange_current_density * 
+                (std::exp(anodic_charge_transfer_coefficient * overpotential / (boltzman_constant*temperature))
+                - std::exp(- cathodic_charge_transfer_coefficient * overpotential / (boltzman_constant*temperature)))
+              )
+            % static_cast<double>(
+                exchange_current_density *
+                (anodic_charge_transfer_coefficient + cathodic_charge_transfer_coefficient)
+                    * overpotential / (boltzman_constant*temperature)
+              )
+            ;
+
     }
 
-    // DISCHARGE
-    rc.reset_voltage(U);
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U_C, U * std::exp(-t/TAU), TOLERANCE);
-        rc.evolve_one_time_step_constant_voltage(DELTA_T, 0.0);
-    }
-}
-
-
-
-BOOST_AUTO_TEST_CASE( test_series_rc_constant_current )
-{
-    double const TAU     = R_SERIES * C;
-    double const DELTA_T = 0.1 * TAU;
-
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
-        time.push_back(t);
-
-    cap::SeriesRC rc(std::make_shared<cap::Parameters>(initialize_database()));
-
-    // CHARGE
-    rc.reset_voltage(0.0);
-    rc.reset_current(I  );
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U, I * (R_SERIES + t / C), TOLERANCE);
-        rc.evolve_one_time_step_constant_current(DELTA_T, I);
-    }
-
-    // DISCHARGE
-    rc.reset_voltage( U);
-    rc.reset_current(-I);
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U, U - I * (R_SERIES + t / C), TOLERANCE);
-        rc.evolve_one_time_step_constant_current(DELTA_T, -I);
-    }
-
-}
-
-
-
-BOOST_AUTO_TEST_CASE( test_series_rc_constant_power )
-{
-    double const TAU     = R_SERIES * C;
-    double const DELTA_T = 0.1 * TAU;
-
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
-        time.push_back(t);
-
-    std::shared_ptr<cap::Parameters const> params =
-        std::make_shared<cap::Parameters const>(initialize_database());
-    cap::SeriesRC rc_newton     (params);
-    cap::SeriesRC rc_fixed_point(params);
-
-    // CHARGE
-    rc_newton     .reset_current(0.0);
-    rc_newton     .reset_voltage(U  );
-    rc_fixed_point.reset_current(0.0);
-    rc_fixed_point.reset_voltage(U  );
-
-    BOOST_FOREACH(double const & t, time)
-    {
-        std::ignore = t; 
-        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
-        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, P, "NEWTON"     );
-        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, P, "FIXED_POINT");
-    }
-
-   BOOST_CHECK_THROW(rc_newton.evolve_one_time_step_constant_power(DELTA_T, P, "INVALID_ROOT_FINDING_METHOD"), std::runtime_error);
-
-    // DISCHARGE
-    rc_newton     .reset_current(0.0);
-    rc_newton     .reset_voltage(U  );
-    rc_fixed_point.reset_current(0.0);
-    rc_fixed_point.reset_voltage(U  );
-    BOOST_FOREACH(double const & t, time)
-    {
-        std::ignore = t; 
-        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
-        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, -P, "NEWTON"     );
-        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, -P, "FIXED_POINT");
-    }
-}
-
-
-
-BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_current )
-{
-    double const TAU     = R_PARALLEL * C;
-    double const DELTA_T = 0.1 * TAU;
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += 0.1*TAU)
-        time.push_back(t);
-
-    cap::ParallelRC rc(std::make_shared<cap::Parameters>(initialize_database()));
-    rc.R_series = 0.0;
-
-    // CHARGE
-    rc.reset_voltage(0.0);
-    rc.reset_current(I  );
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U_C, R_PARALLEL * I * (1.0 - std::exp(-t/(R_PARALLEL*C))), TOLERANCE);
-        rc.evolve_one_time_step_constant_current(DELTA_T, I);
-    }
-
-    // RELAXATION
-    rc.reset_voltage(U  );
-    rc.reset_current(0.0);
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U_C, U * std::exp(-t/(R_PARALLEL*C)), TOLERANCE);
-        rc.evolve_one_time_step_constant_current(DELTA_T, 0.0);
-    }
-}
-
-
-
-BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_voltage )
-{
-    double const TAU     = R_SERIES * C;
-    double const DELTA_T = 0.1 * TAU;
-
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
-        time.push_back(t);
-
-    cap::ParallelRC rc(std::make_shared<cap::Parameters>(initialize_database()));
-
-    // CHARGE
-    rc.reset_voltage(0.0);
-    BOOST_FOREACH(double const & t, time)
-    {
-        BOOST_CHECK_CLOSE(rc.U_C, U * R_PARALLEL/(R_SERIES+R_PARALLEL) * (1.0 - std::exp(-t/((R_SERIES*R_PARALLEL)/(R_SERIES+R_PARALLEL)*C))), TOLERANCE);
-        rc.evolve_one_time_step_constant_voltage(DELTA_T, U);
-    }
-
-}
-
-
-
-BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_power )
-{
-    double const TAU     = R_SERIES * C;
-    double const DELTA_T = 0.1 * TAU;
-
-    std::vector<double> time;
-    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
-        time.push_back(t);
-
-    std::shared_ptr<cap::Parameters const> params =
-        std::make_shared<cap::Parameters const>(initialize_database());
-        std::make_shared<cap::Parameters const>(initialize_database());
-    cap::SeriesRC rc_newton     (params);
-    cap::SeriesRC rc_fixed_point(params);
-
-    // CHARGE
-    rc_newton     .reset_current(0.0);
-    rc_newton     .reset_voltage(U  );
-    rc_fixed_point.reset_current(0.0);
-    rc_fixed_point.reset_voltage(U  );
-
-    BOOST_FOREACH(double const & t, time)
-    {
-        std::ignore = t;
-        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
-        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, P, "NEWTON"     );
-        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, P, "FIXED_POINT");
-    }
-
-    BOOST_CHECK_THROW(rc_newton.evolve_one_time_step_constant_power(DELTA_T, P, "INVALID_ROOT_FINDING_METHOD"), std::runtime_error);
-
-    // DISCHARGE
-    rc_newton     .reset_current(0.0);
-    rc_newton     .reset_voltage(U  );
-    rc_fixed_point.reset_current(0.0);
-    rc_fixed_point.reset_voltage(U  );
-
-    BOOST_FOREACH(double const & t, time)
-    {
-        std::ignore = t;
-        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
-        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
-        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, -P, "NEWTON"     );
-        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, -P, "FIXED_POINT");
-    }
+    fout.close();
 }
---
+++ test/test_resistor_capacitor_circuit.cc
@@ -0,0 +1,249 @@
+#define BOOST_TEST_MODULE ButlerVolmerKinetics
+#define BOOST_TEST_MAIN
+#include <cap/resistor_capacitor.h>
+#include <boost/test/unit_test.hpp>
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <iostream>
+
+
+
+double const R_SERIES   = 55.0e-3;
+double const R_PARALLEL =  2.5e6;
+double const C          =  3.0;
+double const TOLERANCE  =  1.0e-8; // in percentage units
+double const I          =  0.006;
+double const U          =  2.1;
+double const P          =  0.0017;
+
+std::shared_ptr<boost::property_tree::ptree> initialize_database()
+{
+    std::shared_ptr<boost::property_tree::ptree> database =
+        std::make_shared<boost::property_tree::ptree>();
+    database->put("series_resistance"  , R_SERIES  );
+    database->put("parallel_resistance", R_PARALLEL);
+    database->put("capacitance"        , C         );
+    return database;
+}
+
+BOOST_AUTO_TEST_CASE( test_series_rc_constant_voltage )
+{
+    double const TAU     = R_SERIES * C;
+    double const DELTA_T = 0.1 * TAU;
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
+        time.push_back(t);
+
+    cap::SeriesRC rc(std::make_shared<cap::Parameters>(initialize_database()));
+
+    // CHARGE
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U_C, U * (1.0 - std::exp(-t/TAU)), TOLERANCE);
+        rc.evolve_one_time_step_constant_voltage(DELTA_T, U);
+    }
+
+    // DISCHARGE
+    rc.reset_voltage(U);
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U_C, U * std::exp(-t/TAU), TOLERANCE);
+        rc.evolve_one_time_step_constant_voltage(DELTA_T, 0.0);
+    }
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_series_rc_constant_current )
+{
+    double const TAU     = R_SERIES * C;
+    double const DELTA_T = 0.1 * TAU;
+
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
+        time.push_back(t);
+
+    cap::SeriesRC rc(std::make_shared<cap::Parameters>(initialize_database()));
+
+    // CHARGE
+    rc.reset_voltage(0.0);
+    rc.reset_current(I  );
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U, I * (R_SERIES + t / C), TOLERANCE);
+        rc.evolve_one_time_step_constant_current(DELTA_T, I);
+    }
+
+    // DISCHARGE
+    rc.reset_voltage( U);
+    rc.reset_current(-I);
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U, U - I * (R_SERIES + t / C), TOLERANCE);
+        rc.evolve_one_time_step_constant_current(DELTA_T, -I);
+    }
+
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_series_rc_constant_power )
+{
+    double const TAU     = R_SERIES * C;
+    double const DELTA_T = 0.1 * TAU;
+
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
+        time.push_back(t);
+
+    std::shared_ptr<cap::Parameters const> params =
+        std::make_shared<cap::Parameters const>(initialize_database());
+    cap::SeriesRC rc_newton     (params);
+    cap::SeriesRC rc_fixed_point(params);
+
+    // CHARGE
+    rc_newton     .reset_current(0.0);
+    rc_newton     .reset_voltage(U  );
+    rc_fixed_point.reset_current(0.0);
+    rc_fixed_point.reset_voltage(U  );
+
+    BOOST_FOREACH(double const & t, time)
+    {
+        std::ignore = t; 
+        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
+        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, P, "NEWTON"     );
+        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, P, "FIXED_POINT");
+    }
+
+   BOOST_CHECK_THROW(rc_newton.evolve_one_time_step_constant_power(DELTA_T, P, "INVALID_ROOT_FINDING_METHOD"), std::runtime_error);
+
+    // DISCHARGE
+    rc_newton     .reset_current(0.0);
+    rc_newton     .reset_voltage(U  );
+    rc_fixed_point.reset_current(0.0);
+    rc_fixed_point.reset_voltage(U  );
+    BOOST_FOREACH(double const & t, time)
+    {
+        std::ignore = t; 
+        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
+        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, -P, "NEWTON"     );
+        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, -P, "FIXED_POINT");
+    }
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_current )
+{
+    double const TAU     = R_PARALLEL * C;
+    double const DELTA_T = 0.1 * TAU;
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += 0.1*TAU)
+        time.push_back(t);
+
+    cap::ParallelRC rc(std::make_shared<cap::Parameters>(initialize_database()));
+    rc.R_series = 0.0;
+
+    // CHARGE
+    rc.reset_voltage(0.0);
+    rc.reset_current(I  );
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U_C, R_PARALLEL * I * (1.0 - std::exp(-t/(R_PARALLEL*C))), TOLERANCE);
+        rc.evolve_one_time_step_constant_current(DELTA_T, I);
+    }
+
+    // RELAXATION
+    rc.reset_voltage(U  );
+    rc.reset_current(0.0);
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U_C, U * std::exp(-t/(R_PARALLEL*C)), TOLERANCE);
+        rc.evolve_one_time_step_constant_current(DELTA_T, 0.0);
+    }
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_voltage )
+{
+    double const TAU     = R_SERIES * C;
+    double const DELTA_T = 0.1 * TAU;
+
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
+        time.push_back(t);
+
+    cap::ParallelRC rc(std::make_shared<cap::Parameters>(initialize_database()));
+
+    // CHARGE
+    rc.reset_voltage(0.0);
+    BOOST_FOREACH(double const & t, time)
+    {
+        BOOST_CHECK_CLOSE(rc.U_C, 
+            U * R_PARALLEL/(R_SERIES+R_PARALLEL)
+                * (1.0 - std::exp(-t / ((R_SERIES*R_PARALLEL)/(R_SERIES+R_PARALLEL) * C))),
+            TOLERANCE);
+        rc.evolve_one_time_step_constant_voltage(DELTA_T, U);
+    }
+
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_parallel_rc_constant_power )
+{
+    double const TAU     = R_SERIES * C;
+    double const DELTA_T = 0.1 * TAU;
+
+    std::vector<double> time;
+    for (double t = 0.0; t <= 5.0 * TAU; t += DELTA_T)
+        time.push_back(t);
+
+    std::shared_ptr<cap::Parameters const> params =
+        std::make_shared<cap::Parameters const>(initialize_database());
+    cap::SeriesRC rc_newton     (params);
+    cap::SeriesRC rc_fixed_point(params);
+
+    // CHARGE
+    rc_newton     .reset_current(0.0);
+    rc_newton     .reset_voltage(U  );
+    rc_fixed_point.reset_current(0.0);
+    rc_fixed_point.reset_voltage(U  );
+
+    BOOST_FOREACH(double const & t, time)
+    {
+        std::ignore = t;
+        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
+        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, P, "NEWTON"     );
+        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, P, "FIXED_POINT");
+    }
+
+    BOOST_CHECK_THROW(rc_newton.evolve_one_time_step_constant_power(DELTA_T, P, "INVALID_ROOT_FINDING_METHOD"), std::runtime_error);
+
+    // DISCHARGE
+    rc_newton     .reset_current(0.0);
+    rc_newton     .reset_voltage(U  );
+    rc_fixed_point.reset_current(0.0);
+    rc_fixed_point.reset_voltage(U  );
+
+    BOOST_FOREACH(double const & t, time)
+    {
+        std::ignore = t;
+        BOOST_CHECK_CLOSE(rc_newton.U,   rc_fixed_point.U,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.I,   rc_fixed_point.I,   TOLERANCE);
+        BOOST_CHECK_CLOSE(rc_newton.U_C, rc_fixed_point.U_C, TOLERANCE);
+        rc_newton     .evolve_one_time_step_constant_power(DELTA_T, -P, "NEWTON"     );
+        rc_fixed_point.evolve_one_time_step_constant_power(DELTA_T, -P, "FIXED_POINT");
+    }
+}
