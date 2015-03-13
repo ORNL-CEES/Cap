758c3aa6c6a19e5b70a1cc2eeda206b0946c6be9
Damien L-G <qdi@ornl.gov>
2015-03-13 17:39:16 -0400

    updated test leakage current

--- test/test_leakage_current.cc
+++ test/test_leakage_current.cc
@@ -5,20 +5,13 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 #include <iostream>
 #include <fstream>
 
 
 
 namespace cap {
-
-void report(double const t, std::shared_ptr<cap::EnergyStorageDevice const> dev, std::ostream & os = std::cout)
-{
-    os<<boost::format("%10.5f  ") % t;
-    dev->print_data(os);
-}
-
-
 
 void measure_direct_leakage_current(std::shared_ptr<cap::EnergyStorageDevice> dev, std::shared_ptr<boost::property_tree::ptree const> database, std::ostream & os = std::cout)
 {
@@ -27,12 +20,52 @@ void measure_direct_leakage_current(std::shared_ptr<cap::EnergyStorageDevice> de
     double const initial_time        = database->get<double>("initial_time"       );
     double const final_time          = database->get<double>("final_time"         );
     double const time_step           = database->get<double>("time_step"          );
+    double const percent_tolerance   = database->get<double>("percent_tolerance"  );
 
+    double voltage;
+    double current;
+    double current_previous_time_step = std::nan("");
     dev->reset_voltage(initial_voltage);
+    std::size_t step = 0;
     for (double time = initial_time; time < final_time; time += time_step)
     {
+        ++step;
+        dev->get_current(current);
+        dev->get_voltage(voltage);
+        os<<boost::format("%20.15e  %20.15e  %20.15e  \n")
+            % time
+            % current
+            % voltage
+            ;
         dev->evolve_one_time_step_constant_voltage(time_step, dc_voltage);
-        report(time, dev, os);
+        if (boost::test_tools::check_is_close(current, current_previous_time_step, boost::test_tools::percent_tolerance(percent_tolerance)))
+            break;
+        current_previous_time_step = current;
+    }
+}
+
+
+
+void measure_self_discharge(std::shared_ptr<cap::EnergyStorageDevice> dev, std::shared_ptr<boost::property_tree::ptree const> database, std::ostream & os = std::cout)
+{
+    double const initial_time        = database->get<double>("initial_time"       );
+    double const final_time          = database->get<double>("final_time"         );
+    double const time_step           = database->get<double>("time_step"          );
+
+    double voltage;
+    double current;
+    std::size_t step = 0;
+    for (double time = initial_time; time < final_time; time += time_step)
+    {
+        ++step;
+        dev->get_current(current);
+        dev->get_voltage(voltage);
+        os<<boost::format("%20.15e  %20.15e  %20.15e  \n")
+            % time
+            % current
+            % voltage
+            ;
+        dev->evolve_one_time_step_constant_current(time_step, 0.0);
     }
 }
 
@@ -51,11 +84,18 @@ BOOST_AUTO_TEST_CASE( test_leakage_current )
     std::shared_ptr<cap::EnergyStorageDevice> device =
         cap::buildEnergyStorageDevice(std::make_shared<cap::Parameters>(device_database));
 
-    // scan the system
+    // apply a DC voltage to and measure the current required to maintain that voltage
     std::fstream fout;
-    fout.open("leakage_current_data", std::fstream::out);
+    fout.open("direct_leakage_current_data", std::fstream::out);
+    std::shared_ptr<boost::property_tree::ptree> direct_leakage_database =
+        std::make_shared<boost::property_tree::ptree>(input_database->get_child("direct_leakage"));
+    cap::measure_direct_leakage_current(device, direct_leakage_database, fout);
 
-    std::shared_ptr<boost::property_tree::ptree> cyclic_voltammetry_database =
-        std::make_shared<boost::property_tree::ptree>(input_database->get_child("operating_conditions"));
-    cap::measure_direct_leakage_current(device, cyclic_voltammetry_database, fout);
+    // device is charged.  now measure the change of the open circuit potential
+    fout.close();
+    fout.open("self_discharge_data", std::fstream::out);
+    std::shared_ptr<boost::property_tree::ptree> sef_discharge_database =
+        std::make_shared<boost::property_tree::ptree>(input_database->get_child("self_discharge"));
+    cap::measure_self_discharge(device, sef_discharge_database, fout);
+
 }    
