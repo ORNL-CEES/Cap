1b847258dda864a38b3ddff603b63d2bbc0ddcab
Damien L-G <qdi@ornl.gov>
2015-06-26 15:02:53 -0400

    updated utils to be able to read vector of material_ids

--- src/utils.cc
+++ src/utils.cc
@@ -1,4 +1,5 @@
 #include <cap/utils.templates.h>
+#include <deal.II/base/types.h>
 
 namespace cap {
 
@@ -6,6 +7,15 @@ template std::vector<int>         to_vector(std::string const & s);
 template std::vector<float>       to_vector(std::string const & s);
 template std::vector<double>      to_vector(std::string const & s);
 template std::vector<std::string> to_vector(std::string const & s);
+template<>
+std::vector<dealii::types::material_id> to_vector(std::string const & s)
+{
+    std::vector<dealii::types::material_id> v;
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, ',')) { v.push_back(dealii::types::material_id(boost::lexical_cast<dealii::types::material_id>(item))-dealii::types::material_id('0')); }
+    return v;
+}
 
 template std::string to_string(std::vector<int> const         & v);
 template std::string to_string(std::vector<float> const       & v);
