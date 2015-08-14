0d821ce32cfa136c065f34c5de485ee79e029cad
Damien L-G <qdi@ornl.gov>
2015-08-14 13:34:43 -0400

    cleaning cmake dir

--- cmake/cap-data.cmake
+++ cmake/cap-data.cmake
@@ -1,8 +1,6 @@
-EXTERNALPROJECT_ADD(
+ExternalProject_Add(
     cap-data
     GIT_REPOSITORY     https://github.com/dalg24/cap-data.git
-    DOWNLOAD_DIR       ${CAP_DATA_SOURCE_DIR}
-    SOURCE_DIR         ${CAP_DATA_SOURCE_DIR}
     BUILD_IN_SOURCE    0
     CONFIGURE_COMMAND  ""
     BUILD_COMMAND      ""
