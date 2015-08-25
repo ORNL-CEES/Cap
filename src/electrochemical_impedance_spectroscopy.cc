f93cbf11156e6a92637135c08c815d2f600bd5d1
Damien L-G <qdi@ornl.gov>
2015-08-25 08:47:07 -0400

    compute signal to noise ratio

--- src/electrochemical_impedance_spectroscopy.cc
+++ src/electrochemical_impedance_spectroscopy.cc
@@ -22,6 +22,8 @@ compute_fft(double const * input, size_t const n)
     return output;
 }
 
+
+
 std::vector<double>
 compute_fft_frequencies(size_t const n, double const frequency)
 {
@@ -31,12 +33,41 @@ compute_fft_frequencies(size_t const n, double const frequency)
     return fft_frequencies;
 }
 
+
+
+std::vector<double>
+compute_signal_to_noise_ratio(std::vector<std::complex<double>> fft_data,
+    std::vector<int> excited_harmonics, std::vector<int> unexcited_harmonics)
+{
+    assert(fft_data.size() == excited_harmonics.size() + unexcited_harmonics.size() + 1);
+    double dc_power = std::norm(fft_data[0]);
+    double ac_power = 0.0;
+    for (int k : excited_harmonics)
+        ac_power += std::norm(fft_data[k]);
+    double noise_power = 0.0;
+    for (int k : unexcited_harmonics)
+        noise_power += std::norm(fft_data[k]);
+    double total_power = dc_power + ac_power + noise_power;
+    std::ignore = total_power;
+    // TODO:  definition is a bit shaky for multiple frequency
+    // should compute for each individual excited freq vs nearby unex freq
+    std::vector<double> ratios;
+    for (size_t k : excited_harmonics) {
+        ratios.emplace_back(std::norm(fft_data[k])/ noise_power);
+    }
+    return ratios;
+}
+
+
+
 std::function<void(std::shared_ptr<cap::EnergyStorageDevice>,double,double)>
 get_evolve_one_time_step(std::shared_ptr<boost::property_tree::ptree const> database)
 {
     std::vector<int   > const harmonics  = cap::to_vector<int   >(database->get<std::string>("harmonics" ));
     std::vector<double> const amplitudes = cap::to_vector<double>(database->get<std::string>("amplitudes"));
     std::vector<double> const phases     = cap::to_vector<double>(database->get<std::string>("phases"    ));
+    assert(harmonics.size() == amplitudes.size());
+    assert(harmonics.size() == phases.size());
     double const frequency = database->get<double>("frequency");
     auto compute_ac_excitation_signal =
         [frequency,harmonics,amplitudes,phases](double time)
@@ -68,8 +99,11 @@ get_evolve_one_time_step(std::shared_ptr<boost::property_tree::ptree const> data
     }
 }
 
+
+
 std::map<double,std::complex<double>>
-measure_impedance(std::shared_ptr<cap::EnergyStorageDevice> device, std::shared_ptr<boost::property_tree::ptree const> database)
+measure_impedance(std::shared_ptr<cap::EnergyStorageDevice> device,
+std::shared_ptr<boost::property_tree::ptree const> database)
 {
     std::vector<int   > const harmonics  = cap::to_vector<int   >(database->get<std::string>("harmonics" ));
     double const frequency       = database->get<double>("frequency"    );
@@ -99,14 +133,13 @@ measure_impedance(std::shared_ptr<cap::EnergyStorageDevice> device, std::shared_
     std::vector<double> fft_frequencies =
         compute_fft_frequencies((cycles-ignore_cycles)*steps_per_cycle, frequency/(cycles-ignore_cycles));
 
-    // check signal-to-noise ratio
+    //
     int const n = (cycles-ignore_cycles)*steps_per_cycle;
     assert(fft_current.size() == n/2+1);
     assert(fft_voltage.size() == n/2+1);
     assert(fft_frequencies.size() == n/2+1);
     std::vector<int> excited_harmonics;
     std::vector<int> unexcited_harmonics;
-// TODO
     for (int i = 1; i < n/2; ++i)
         if ((std::find(harmonics.begin(), harmonics.end(), i/(cycles-ignore_cycles)) != harmonics.end())
             && (i%(cycles-ignore_cycles) == 0))
@@ -114,24 +147,13 @@ measure_impedance(std::shared_ptr<cap::EnergyStorageDevice> device, std::shared_
         else
             unexcited_harmonics.push_back(i);
 
-//    double dc_power = std::pow(data[0], 2);
-//    double ac_power = 0.0;
-//    for (size_t k : excited_harmonics)
-//        ac_power += std::pow(data[k], 2) + std::pow(data[n-k], 2);
-//    double noise_power = std::pow(data[n/2], 2);
-//    for (size_t k : unexcited_harmonics)
-//        noise_power += std::pow(data[k], 2) + std::pow(data[n-k], 2);
-//    double total_power = dc_power + ac_power + noise_power;
-//    std::ignore = total_power;
-//    // TODO:  definition is a bit shaky for multiple frequency
-//    // should compute for each individual excited freq vs nearby unex freq
-//    std::vector<double> ratios;
-//    double harmonics_power;
-//    for (size_t k : excited_harmonics) {
-//        harmonics_power = std::pow(data[k], 2) + std::pow(data[n-k], 2);
-//        ratios.push_back(harmonics_power / noise_power);
-//    }
-     
+    // check signal-to-noise ratio
+    std::vector<double> current_signal_to_noise_ratio =
+        compute_signal_to_noise_ratio(fft_current, excited_harmonics, unexcited_harmonics);
+    std::vector<double> voltage_signal_to_noise_ratio =
+        compute_signal_to_noise_ratio(fft_voltage, excited_harmonics, unexcited_harmonics);
+    // TODO: do we want to assert something or give a warning if ratio is not good enough?
+
     // return complex impedance
     std::map<double,std::complex<double>> impedance;
     for (int const & k : excited_harmonics)
