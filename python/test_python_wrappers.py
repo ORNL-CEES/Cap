23c1729400366b7f0f6097c5aaec0bcf4c8f68cd
Damien L-G <qdi@ornl.gov>
2015-08-25 08:43:55 -0400

    added measure impedance to python wrappers

--- python/test_python_wrappers.py
+++ python/test_python_wrappers.py
@@ -20,7 +20,9 @@ def evolve_constant_voltage(device,steps,time_step,constant_voltage):
 
 class constantChargeTestCase(unittest.TestCase):
     def test_is_five_prime(self):
-        device=pycap.EnergyStorageDevice("device.xml")
+        device_database=pycap.PropertyTree()
+        device_database.parse_xml('device.xml')
+        device=pycap.EnergyStorageDevice(device_database)
         steps=1000
         time_step=0.01
         constant_voltage=2.0
@@ -40,10 +42,33 @@ class boostPropertyTreePythonWrappersTestCase(unittest.TestCase):
         self.assertEqual(ptree.get_string('good.news'),'it works')
 
 class capEnergyStorageDeviceWrappersTestCase(unittest.TestCase):
-    def test_energy_storage_device(self):
+    def test_energy_storage_device_parse_xml_input(self):
         device_database=pycap.PropertyTree()
         device_database.parse_xml('device.xml')
-#        device=pycap.EnergyStorageDevice(device_database)
+        device=pycap.EnergyStorageDevice(device_database)
+    def test_energy_storage_device_build_datbase(self):
+        device_database=pycap.PropertyTree()
+        device_database.put_string('device.type','SeriesRC')
+        device_database.put_double('device.capacitance',3.0)
+        device_database.put_double('device.series_resistance',100.0)
+        device=pycap.EnergyStorageDevice(device_database)
+    def test_measure_impedance(self):
+        device_database=pycap.PropertyTree()
+        device_database.parse_xml('device.xml')
+        device=pycap.EnergyStorageDevice(device_database)
+        eis_database=pycap.PropertyTree()
+        eis_database.parse_xml('eis.xml')
+        data=pycap.ElectrochemicalImpedanceSpectroscopyData(device,eis_database)
+        freq=numpy.array(data.get_frequencies())
+        impedance=numpy.array(data.get_complex_impedance())
+        print freq
+        print impedance
+        w=2*numpy.pi*freq
+        C=device_database.get_double('device.capacitance')
+        Rs=device_database.get_double('device.series_resistance')
+        Rl=device_database.get_double('device.parallel_resistance')
+        print Rs+Rl/(1+1j*Rl*C*w)
+
 
 if __name__ == '__main__':
     unittest.main()
