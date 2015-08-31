3601d180ce4eba0615d00951bc4f96d920b28e35
Damien L-G <qdi@ornl.gov>
2015-08-31 14:47:21 -0400

    updated test python

--- python/test_python_wrappers.py
+++ python/test_python_wrappers.py
@@ -55,29 +55,48 @@ class capEnergyStorageDeviceWrappersTestCase(unittest.TestCase):
         device_database=pycap.PropertyTree()
         device_database.parse_xml('device.xml')
         device=pycap.EnergyStorageDevice(device_database)
-    def test_energy_storage_device_build_datbase(self):
+    def test_energy_storage_device_build_database(self):
+        def build_energy_storage_device(device_database):
+            device=pycap.EnergyStorageDevice(device_database)
+            return device
         device_database=pycap.PropertyTree()
+        # build a series rc equivalent circuit database
         device_database.put_string('device.type','SeriesRC')
         device_database.put_double('device.capacitance',3.0)
         device_database.put_double('device.series_resistance',100.0)
+        # change it to a parallel rc database
+        device_database.put_string('device.type','ParallelRC')
+        # database is incomplete at this time so building a device will throw
+        self.assertRaises(RuntimeError,build_energy_storage_device,device_database)
+        def throw_exception_incomplete_database(device_database):
+            device=pycap.EnergyStorageDevice(device_database)
+        # add the missing information to the database
+        device_database.put_double('device.parallel_resistance',4000.0)
+        # build the new energy device
         device=pycap.EnergyStorageDevice(device_database)
     def test_measure_impedance(self):
+        # build an energy storage device
         device_database=pycap.PropertyTree()
         device_database.parse_xml('device.xml')
         device=pycap.EnergyStorageDevice(device_database)
+        # measure its impedance
         eis_database=pycap.PropertyTree()
         eis_database.parse_xml('eis.xml')
         data=pycap.ElectrochemicalImpedanceSpectroscopyData(device,eis_database)
         freq=numpy.array(data.get_frequencies())
         impedance=numpy.array(data.get_complex_impedance())
-        print freq
-        print impedance
         w=2*numpy.pi*freq
+        # the following assume the device is a parallel rc circuit
+        self.assertEqual(device_database.get_string('device.type'),'ParallelRC')
         C=device_database.get_double('device.capacitance')
         Rs=device_database.get_double('device.series_resistance')
         Rl=device_database.get_double('device.parallel_resistance')
-        print Rs+Rl/(1+1j*Rl*C*w)
-
+        # compute theoretical impedance
+        Z=Rs+Rl/(1+1j*Rl*C*w)
+        # check that the relative error is less than .01%
+        error_norm=numpy.linalg.norm((impedance-Z)/impedance,ord=numpy.inf)
+        percent_tolerance=1.0e-2
+        self.assertLessEqual(error_norm,percent_tolerance)
 
 if __name__ == '__main__':
     unittest.main()
