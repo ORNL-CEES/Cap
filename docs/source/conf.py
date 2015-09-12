ad28be428107cfb583cc433a5178c97ba319e4ce
dalg24 <dalg24@gmail.com>
2015-09-12 00:09:36 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.doctest',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
