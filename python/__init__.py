a96b9323ceb8b03268824163c7de577dcbfc7920
Damien L-G <qdi@ornl.gov>
2015-09-18 10:13:57 -0400

    forwarding python package version info

--- python/__init__.py
+++ python/__init__.py
@@ -2,4 +2,8 @@ __all__=['_pycap','extra']
 
 from _pycap import *
 
+__version__=_pycap.__version__
+__git_branch__=_pycap.__git_branch__
+__git_commit_hash__=_pycap.__git_commit_hash__
+
 from .extra import Greeting
