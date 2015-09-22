6f2e6e2e9b175620342c325b36f23292616867a3
Damien L-G <qdi@ornl.gov>
2015-09-22 17:00:52 -0400

    update test stage/multistage

--- python/test/test_stage.py
+++ python/test/test_stage.py
@@ -1,4 +1,6 @@
-from pycap import PropertyTree,EnergyStorageDevice,Stage,initialize_data
+from pycap import PropertyTree,EnergyStorageDevice
+from pycap import Stage,MultiStage
+from pycap import initialize_data
 import unittest
 
 device_database=PropertyTree()
@@ -17,6 +19,7 @@ class capStageTestCase(unittest.TestCase):
         data=initialize_data()
         steps=stage.run(device,data)
         self.assertEqual(steps,150)
+        self.assertEqual(steps,len(data['time']))
         self.assertAlmostEqual(data['time'][-1],15.0)
         self.assertAlmostEqual(data['current'][-1],5e-3)
     def test_force_discharge(self):
@@ -30,8 +33,29 @@ class capStageTestCase(unittest.TestCase):
         data=initialize_data()
         steps=stage.run(device,data)
         self.assertGreaterEqual(steps,1)
+        self.assertEqual(steps,len(data['time']))
         self.assertAlmostEqual(data['voltage'][-1],0.0)
         self.assertLessEqual(data['current'][-1],1e-5)
+    def test_time_steps(self):
+        ptree=PropertyTree()
+        ptree.put_int('stages',2)
+        ptree.put_int('cycles',1)
+        ptree.put_double('time_step',1.0)
+        ptree.put_string('stage_0.mode','hold')
+        ptree.put_string('stage_0.end_criterion','time')
+        ptree.put_double('stage_0.duration',2.0)
+        ptree.put_string('stage_1.mode','rest')
+        ptree.put_string('stage_1.end_criterion','time')
+        ptree.put_double('stage_1.duration',1.0)
+        ptree.put_double('stage_1.time_step',0.1)
+        multi=MultiStage(ptree)
+        data=initialize_data()
+        steps=multi.run(device,data)
+        self.assertEqual(steps,12)
+        self.assertEqual(steps,len(data['time']))
+        self.assertAlmostEqual(data['time'][5],2.4)
+        self.assertAlmostEqual(data['voltage'][0],data['voltage'][1])
+        self.assertAlmostEqual(data['current'][3],0.0)
 
 if __name__ == '__main__':
     unittest.main()
