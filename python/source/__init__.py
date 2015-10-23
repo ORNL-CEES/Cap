5b58193bfaa4e7303f1e0aa0ee7bb6308fd3a145
Damien L-G <qdi@ornl.gov>
2015-10-23 12:11:29 -0400

    added cyclic voltammetry to python

--- python/source/__init__.py
+++ python/source/__init__.py
@@ -1,4 +1,4 @@
-__all__=['_pycap','data_helpers','time_evolution','end_criterion','stage','charge_discharge',
+__all__=['_pycap','data_helpers','time_evolution','end_criterion','stage','charge_discharge','voltammetry'
     'ragone_plot','impedance_spectroscopy'
 ]
 
@@ -13,5 +13,6 @@ from .time_evolution import *
 from .end_criterion import *
 from .stage import *
 from .charge_discharge import *
+from .voltammetry import *
 from .ragone_plot import *
 from .impedance_spectroscopy import *
