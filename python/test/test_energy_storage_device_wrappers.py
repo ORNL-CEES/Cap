1b8a3a28ee4492cd825fbd239a63c832fa244589
Damien L-G <qdi@ornl.gov>
2015-12-04 20:28:52 -0500

    update test energy storage device python wrap

--- python/test/test_energy_storage_device_wrappers.py
+++ python/test/test_energy_storage_device_wrappers.py
@@ -2,22 +2,23 @@ from pycap import PropertyTree,EnergyStorageDevice
 from numpy import array,pi,linalg,inf
 import unittest
 
+valid_device_input=[
+    "series_rc.info",
+    "parallel_rc.info",
+#    "super_capacitor.info",
+    ]
+
 class capEnergyStorageDeviceWrappersTestCase(unittest.TestCase):
-    def test_energy_storage_device_build_database(self):
-        device_database=PropertyTree()
-        # build a series rc equivalent circuit database
-        device_database.put_string('type','SeriesRC')
-        device_database.put_double('capacitance',3.0)
-        device_database.put_double('series_resistance',100.0)
-        device=EnergyStorageDevice(device_database)
-        # change it to a parallel rc database
-        device_database.put_string('type','ParallelRC')
-        # database is incomplete at this time so building a device will throw
-        self.assertRaises(RuntimeError,EnergyStorageDevice,device_database)
-        # add the missing information to the database
-        device_database.put_double('parallel_resistance',4000.0)
-        # build the new energy device
-        device=EnergyStorageDevice(device_database)
+    def test_energy_storage_device_factory(self):
+        # valid input to buid an energy storage device
+        for filename in valid_device_input:
+            ptree=PropertyTree()
+            ptree.parse_info(filename)
+            device=EnergyStorageDevice(ptree)
+        # invalid device will throw an exception
+        ptree=PropertyTree()
+        ptree.put_string('type', 'InvalidDevice')
+        self.assertRaises(RuntimeError,EnergyStorageDevice,ptree)
     def test_energy_storage_device_not_copyable(self):
         device_database=PropertyTree()
         device_database.parse_xml('device.xml')
