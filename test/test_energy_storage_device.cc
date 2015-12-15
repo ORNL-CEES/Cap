1ff66adec456abfb9160425853009b3acd2542f7
Damien L-G <qdi@ornl.gov>
2015-12-15 16:39:36 -0500

    add test/example of simple device inspector

--- test/test_energy_storage_device.cc
+++ test/test_energy_storage_device.cc
@@ -25,24 +25,62 @@ std::list<std::string> const valid_device_input = {
 #endif
     };
 
-BOOST_AUTO_TEST_CASE( test_factory )
+BOOST_AUTO_TEST_CASE( test_energy_storage_device_builders )
 {
+    boost::mpi::communicator world;
     for (auto const & filename : valid_device_input)
     {
         boost::property_tree::ptree ptree;
         boost::property_tree::info_parser::read_info(filename, ptree);
         BOOST_CHECK_NO_THROW(
-            cap::EnergyStorageDevice::build(
-                boost::mpi::communicator(), ptree) );
+            cap::EnergyStorageDevice::build(world, ptree) );
     }
 
     // invalid type must throw an exception
     boost::property_tree::ptree ptree;
     ptree.put("type", "InvalidDeviceType");
     BOOST_CHECK_THROW(
-        cap::buildEnergyStorageDevice(
-            boost::mpi::communicator(), ptree ),
+        cap::EnergyStorageDevice::build(world, ptree),
         std::runtime_error );
+}
+
+class ExampleInspector : public cap::EnergyStorageDeviceInspector
+{
+public:
+    // get the device type and set the voltage to 1.4 volt
+    void inspect(cap::EnergyStorageDevice * device)
+    {
+        // use dynamic_cast to find out the actual type
+        if (dynamic_cast<cap::SeriesRC*>(device) != nullptr)
+            _type = "SeriesRC";
+        else if (dynamic_cast<cap::ParallelRC*>(device) != nullptr)
+            _type = "ParallelRC";
+        else
+            throw std::runtime_error("not an equivalent circuit model");
+        // if we make ExampleInspector friend of the derived
+        // class for the EnergyStorageDevice we could virtually
+        // do anything
+        device->evolve_one_time_step_constant_voltage(1.0, 1.4);
+    }
+    // the type of the device last inspected
+    std::string _type;
+};
+
+BOOST_AUTO_TEST_CASE( test_energy_storage_device_inspectors )
+{
+    std::string const filename = "series_rc.info";
+    boost::mpi::communicator world;
+    boost::property_tree::ptree ptree;
+    boost::property_tree::info_parser::read_info(filename, ptree);
+    auto device = cap::EnergyStorageDevice::build(world, ptree);
+    double voltage;
+    device->get_voltage(voltage);
+    BOOST_TEST( voltage != 1.4 );
+    ExampleInspector inspector;
+    device->inspect(&inspector);
+    BOOST_TEST( (inspector._type).compare("SeriesRC") == 0 );
+    device->get_voltage(voltage);
+    BOOST_TEST( voltage == 1.4 );
 }
 
 // TODO: won't work for SuperCapacitor
