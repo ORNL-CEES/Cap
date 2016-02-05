d596edea5618c474e666c9dc8648ed1e7ee69462
Damien L-G <qdi@ornl.gov>
2016-02-05 13:43:25 -0500

    run the cpp tests

--- docker/build_cap.sh
+++ docker/build_cap.sh
@@ -13,6 +13,10 @@ cmake \
     -D DEAL_II_INSTALL_DIR=${DEAL_II_DIR} \
     -D CAP_DATA_DIR=${PREFIX}/source/cap-data \
     ${PREFIX}/source/cap && \
-make -j4 install && \
-cd python && \
+make -j4 && \
+useradd -m -s /bin/bash -N -u 1000 jovyan && \
+chown jovyan ${PREFIX}/build/cap -R && \
+su jovyan <<EOF
+export LD_LIBRARY_PATH=${BOOST_DIR}/lib:$LD_LIBRARY_PATH
 ctest -j4 -V
+EOF
