814744b96478712d99389b117451c2544d0ccee9
Damien L-G <qdi@ornl.gov>
2016-03-13 22:19:36 -0400

    produce coverage report
    configure with ``-D ENABLE_COVERAGE=ON``
    run ``make coverage-cpp`` and ``make coverage-python``

--- docker/build_cap.sh
+++ docker/build_cap.sh
@@ -4,10 +4,13 @@ cd ${PREFIX}/source && \
 git clone https://github.com/dalg24/cap-data.git && \
 mkdir -p ${PREFIX}/build/cap && \
 cd ${PREFIX}/build/cap && \
+apt-get install -y lcov && \
+pip install codecov && \
 cmake \
     -D CMAKE_INSTALL_PREFIX=/opt/cap \
     -D CMAKE_BUILD_TYPE=Debug \
     -D BUILD_SHARED_LIBS=ON \
+    -D ENABLE_COVERAGE=ON \
     -D ENABLE_PYTHON=ON \
     -D BOOST_DIR=${BOOST_DIR} \
     -D ENABLE_DEAL_II=ON \
@@ -20,6 +23,10 @@ make -j4 VERBOSE=1 && \
 useradd -m -s /bin/bash -N -u 1000 jovyan && \
 chown jovyan ${PREFIX}/build/cap -R && \
 su jovyan <<EOF
-export LD_LIBRARY_PATH=${BOOST_DIR}/lib:$LD_LIBRARY_PATH
-ctest -j4 -V
+export LD_LIBRARY_PATH=${BOOST_DIR}/lib:$LD_LIBRARY_PATH && \
+export PATH=${PYTHON_DIR}/bin:${PATH} && \
+ctest -j4 -V && \
+make coverage-cpp && make coverage-python && \
+cd ${PREFIX}/source/cap && \
+codecov --disable gcov search pycov --file ${PREFIX}/build/cap/lcov.info ${PREFIX}/build/cap/coverage.xml
 EOF
