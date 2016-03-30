dbb57ea8e927f689941d643bb4316ef62efbf0fa
Damien L-G <qdi@ornl.gov>
2016-03-30 17:17:38 -0400

    Improve build info on CDash

--- docker/get_build_info.sh
+++ docker/get_build_info.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "TODO"
+    curl -s "https://api.travis-ci.org/repos/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}" | jq '.message' | sed 's/\\n.*//'
 else
-    export PULL_REQUEST_TITLE=$(curl -s "https://api.github.com/repos/ORNL-CEES/Cap/pulls/${TRAVIS_PULL_REQUEST}" | jq '.title' | sed 's/"//g')
-    echo "Pull Request #${TRAVIS_PULL_REQUEST}: ${PULL_REQUEST_TITLE}"
+    export PULL_REQUEST_TITLE=$(curl -s "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}" | jq '.title' | sed 's/"//g')
+    echo "PR #${TRAVIS_PULL_REQUEST}: ${PULL_REQUEST_TITLE}"
 fi
 
