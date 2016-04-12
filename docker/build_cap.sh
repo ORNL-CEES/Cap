ad6f714132883456738c1ff3f8c0d663331d7a55
Damien L-G <qdi@ornl.gov>
2016-04-12 11:37:07 -0400

    Cleanup travis build script

--- docker/build_cap.sh
+++ docker/build_cap.sh
@@ -8,16 +8,6 @@ git clone https://github.com/dalg24/cap-data.git ${PREFIX}/source/cap-data
 mv ${MPI_DIR}/bin/mpiexec ${MPI_DIR}/bin/mpiexec.alias
 echo '#!/usr/bin/env bash\nmpiexec.alias --allow-run-as-root "$@"' > ${MPI_DIR}/bin/mpiexec
 chmod +x ${MPI_DIR}/bin/mpiexec
-# setup report to cdash
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    DASHBOARD=Continuous
-else
-    DASHBOARD=Experimental
-fi
-echo 'set(CTEST_DROP_METHOD "http")' >>  ${PREFIX}/source/cap/CTestConfig.cmake
-echo 'set(CTEST_DROP_SITE "jupyterdocker.ornl.gov/CDash")' >> ${PREFIX}/source/cap/CTestConfig.cmake
-echo 'set(CTEST_DROP_LOCATION "/submit.php?project=Cap")' >> ${PREFIX}/source/cap/CTestConfig.cmake
-echo 'set(CTEST_DROP_SITE_CDASH TRUE)' >>  ${PREFIX}/source/cap/CTestConfig.cmake
 # build the code
 mkdir ${PREFIX}/build/cap
 cd ${PREFIX}/build/cap
@@ -36,8 +26,6 @@ cmake \
     -D CAP_DATA_DIR=${PREFIX}/source/cap-data \
     -D ENABLE_COVERAGE=ON \
     -D ENABLE_FORMAT=ON \
-    -D SITE="travis-ci" \
-    -D BUILDNAME="\"${CTEST_BUILD_NAME}\"" \
     ${PREFIX}/source/cap
 make -j${NPROC} -i
 # run unit tests
