f1e6bd659fa227e2ee5bbf1e5e46a7ec675d9b5c
Damien L-G <qdi@ornl.gov>
2016-07-13 18:36:53 -0400

    Disable check Python format when Python is not enabled

--- cmake/CodeFormat.cmake
+++ cmake/CodeFormat.cmake
@@ -52,44 +52,46 @@ add_test(
 )
 
 ## Python format ##############################################################
-find_program(AUTOPEP8_EXECUTABLE autopep8)
-if(AUTOPEP8_EXECUTABLE)
-    message("-- Found autopep8: ${AUTOPEP8_EXECUTABLE}")
-else()
-    message(FATAL_ERROR "-- autopep8 not found")
+if(ENABLE_PYTHON)
+  find_program(AUTOPEP8_EXECUTABLE autopep8)
+  if(AUTOPEP8_EXECUTABLE)
+      message("-- Found autopep8: ${AUTOPEP8_EXECUTABLE}")
+  else()
+      message(FATAL_ERROR "-- autopep8 not found")
+  endif()
+  add_custom_target(format-python
+      ${AUTOPEP8_EXECUTABLE}
+      --in-place --recursive
+      ${CMAKE_SOURCE_DIR}/python
+  )
+  file(WRITE
+      ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/check_format_python.sh
+      "#!/usr/bin/env bash\n"
+      "\n"
+      "${AUTOPEP8_EXECUTABLE} "
+      "--diff --recursive "
+      "${CMAKE_SOURCE_DIR}/python "
+      "| tee diff\n"
+      "diff=`cat diff`\n"
+      "rm diff\n"
+      "if [ -z \"$diff\" ]; then\n"
+      "    echo OK\n"
+      "    exit 0\n"
+      "else\n"
+      "    exit 1\n"
+      "fi"
+  )
+  file(COPY
+      ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/check_format_python.sh
+      DESTINATION
+          ${CMAKE_BINARY_DIR}
+      FILE_PERMISSIONS
+          OWNER_READ OWNER_WRITE OWNER_EXECUTE
+          GROUP_READ GROUP_EXECUTE
+          WORLD_READ WORLD_EXECUTE
+  )
+  add_test(
+      NAME check_format_py
+      COMMAND ${CMAKE_BINARY_DIR}/check_format_python.sh
+  )
 endif()
-add_custom_target(format-python
-    ${AUTOPEP8_EXECUTABLE} 
-    --in-place --recursive
-    ${CMAKE_SOURCE_DIR}/python
-)
-file(WRITE
-    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/check_format_python.sh
-    "#!/usr/bin/env bash\n"
-    "\n"
-    "${AUTOPEP8_EXECUTABLE} "
-    "--diff --recursive "
-    "${CMAKE_SOURCE_DIR}/python "
-    "| tee diff\n"
-    "diff=`cat diff`\n"
-    "rm diff\n"
-    "if [ -z \"$diff\" ]; then\n"
-    "    echo OK\n"
-    "    exit 0\n"
-    "else\n"
-    "    exit 1\n"
-    "fi"
-)
-file(COPY
-    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/check_format_python.sh
-    DESTINATION
-        ${CMAKE_BINARY_DIR}
-    FILE_PERMISSIONS
-        OWNER_READ OWNER_WRITE OWNER_EXECUTE
-        GROUP_READ GROUP_EXECUTE
-        WORLD_READ WORLD_EXECUTE
-)
-add_test(
-    NAME check_format_py
-    COMMAND ${CMAKE_BINARY_DIR}/check_format_python.sh
-)
