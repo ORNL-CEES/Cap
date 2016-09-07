7bda1b3432f1816a9f0785eecc34e743537cfff8
Damien L-G <qdi@ornl.gov>
2016-09-07 16:36:44 -0400

    Apply pep8 on helpers.py

--- python/example/helpers.py
+++ python/example/helpers.py
@@ -5,21 +5,25 @@ from matplotlib import pyplot
 from sys import stdout, exit
 from os import remove
 
+
 class PrintColumns(Observer):
+
     def __new__(cls, *args, **kwargs):
         return object.__new__(PrintColumns)
+
     def __init__(self):
         self._template = u''
         for i in range(3):
             self._template += '{left}{0}:{format_spec}{right}{separator}'\
                 .format(i, format_spec='{format_spec}',
                         left='{', right='}', separator='\t')
+
     def update(self, subject, *args, **kwargs):
         extra = '>20'
         print(self._template.format('freq/Hz',
                                     'Re(Z)/ohm',
                                     '-Im(Z)/ohm',
-                                    format_spec=extra+"s"),
+                                    format_spec=extra + "s"),
               file=stdout)
         n = subject._data['frequency'].size
         for i in range(n):
@@ -29,16 +33,20 @@ class PrintColumns(Observer):
             place_holder = 255
             line = self._template.format(float(f),
                                          float(real(Z)),
-                                         -float(imag(Z)),                                   
-                                         format_spec=extra+'.7e')
+                                         -float(imag(Z)),
+                                         format_spec=extra + '.7e')
             print(line, file=stdout)
 
+
 class RefreshDisplay(Observer):
+
     def __new__(cls, *args, **kwargs):
         return object.__new__(RefreshDisplay)
+
     def update(self, subject, *args, **kwargs):
         display.clear_output(wait=True)
         display.display(pyplot.gcf())
+
 
 def check_input(device, experiment):
     experiment._extra_data = device.inspect()
